1483dd5f196a7aab11005a72630f1813219321b0
Black Web <109763652+bl4ckweb@users.noreply.github.com>
2022-08-30 15:34:08 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const btn = document.getElementById('submit');
+
+function whatsapp()
+{
+    let nombre = document.getElementById('nombre').value;
+    let correo = document.getElementById('correo').value;
+    let telefono = document.getElementById('telefono').value;
+    let mensaje = document.getElementById('mensaje').value;
+
+    var win = window.open(`https://api.whatsapp.com/send?phone=5172695863&text=Hola%20me%20llamo%20${nombre}%20,mi%20correo%20es%20${correo}%20,mi%20teléfono%20es%20${telefono}%20y%20${mensaje}`,'_blank')
+}
+
